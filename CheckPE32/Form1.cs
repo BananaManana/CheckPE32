5c08a13d566d5d6101f6839a70e355268ba7ec98
BananaManana <elenameet@gmail.com>
2014-05-23 23:00:48 +0400

    Сделан поиск сигнатур

--- CheckPE32/Form1.cs
+++ CheckPE32/Form1.cs
@@ -16,23 +16,56 @@ namespace CheckPE32
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// функция возвращает индекс массива arraySource, с которого начинается массив arrayPattern
+        /// </summary>
+        /// <param name="arraySource">исходный массив</param>
+        /// <param name="arrayPattern">искомый массив</param>
+        /// <param name="startInd">индекс в исходном массиве, с которого надо начать поиск</param>
+        /// <param name="count">кол-во элементов в исходном массиве, которое надо проверить</param>
+        /// <returns>индекс элемента в исходном массиве, с которого начинается искомый массив</returns>
+        public static int FindArray(byte[] arraySource, byte[] arrayPattern, int startInd, int count)
+        {
+            if (count >= arrayPattern.Length && (startInd + count) <= arraySource.Length)
+            {
+                int maxIndNext = count + startInd;
+                int delta = maxIndNext - arrayPattern.Length;
+                for (int i = startInd; i < maxIndNext; ++i)
+                {
+                    if (arrayPattern[0] == arraySource[i] && i <= delta)
+                    {
+                        int k = 1;
+                        for (int j = 1; j < arrayPattern.Length; ++j)
+                        {
+                            if (arrayPattern[j] == arraySource[i + k])
+                                k++;
+                            else
+                                break;
+                            if (k == arrayPattern.Length)
+                                return i;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
 
         public bool CheckFile(string fileName)
         {
             byte[] data = System.IO.File.ReadAllBytes(textBox1.Text);
             //необходимо найти сигнатуру 50450000h
-            string sData = Encoding.Unicode.GetString(data, 0, data.Length);
-            int offset = sData.IndexOf("PE  ");
-           
+            //string sData = Encoding.Unicode.GetString(data, 0, data.Length);
+            byte[] signature = {0x50, 0x45, 0x00, 0x00};
+            int offset = FindArray(data, signature, 0, data.Length);
+                    
             //offset += 88; //смещение 58h - значение по этому смещению хранит контрольную сумму файла
             offset += 0x98; //98h - первое из значений, определяющих наличие подписи (4 значения наоборот)
                             //Это 4-байтное значение означает смещение, по которому находится начало цифровой подписи
                             //offset += 0x9C; //9Ch - второе из значений, определяющих наличие подписи (4 значения наоборот)
                             //Оно означает размер цифровой подписи
-            int i = 0;
             bool isNotSigned = true;
             //далее проверяется последовательность из 8 байтов, если все они равны нулю - файл не подписан
-            for (i = 0; i < 7; i++)
+            for (int i = 0; i < 7; i++)
             {
                 if (data[offset + i] != 0) isNotSigned = false;
             };
